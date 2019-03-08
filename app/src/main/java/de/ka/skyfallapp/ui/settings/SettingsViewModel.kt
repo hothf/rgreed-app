eaf98ffdc8219412ec3a4ead433ad58a5b2bf043
Thomas Hofmann <thomas.hofmann@jamitlabs.com>
2019-03-08 11:58:35 +0100

    Clean up settings

--- app/src/main/java/de/ka/skyfallapp/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/de/ka/skyfallapp/ui/settings/SettingsViewModel.kt
@@ -1,10 +1,43 @@
 package de.ka.skyfallapp.ui.settings
 
 import android.app.Application
+import android.widget.CompoundButton
+import androidx.lifecycle.MutableLiveData
+import de.ka.skyfallapp.BuildConfig
 import de.ka.skyfallapp.R
 import de.ka.skyfallapp.base.BaseViewModel
+import de.ka.skyfallapp.repo.Profile
+import de.ka.skyfallapp.utils.AndroidSchedulerProvider
+import de.ka.skyfallapp.utils.with
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
 
 class SettingsViewModel(app: Application) : BaseViewModel(app) {
+
+    var pushEnabled = true
+
+    val loginText = app.getString(R.string.settings_login)
+    val isPushEnabled = MutableLiveData<Boolean>().apply { value = pushEnabled }
+    val profileText = MutableLiveData<String>().apply { value = loginText }
+    val versionText = "${BuildConfig.VERSION_NAME}.${BuildConfig.BUILD_TYPE}"
+    val pushCheckedChangeListener = CompoundButton.OnCheckedChangeListener { _, checked ->
+        pushEnabled = checked
+        isPushEnabled.postValue(checked)
+    }
+
+    init {
+        changeToProfile(repository.profileManager.currentProfile)
+
+        repository.profileManager.observableProfile
+            .with(AndroidSchedulerProvider())
+            .subscribeBy(onNext = { changeToProfile(it) })
+            .addTo(compositeDisposable)
+    }
+
+
+    private fun changeToProfile(profile: Profile?) {
+        profileText.postValue(profile?.username ?: loginText)
+    }
 
 
     fun onProfileClicked() {
