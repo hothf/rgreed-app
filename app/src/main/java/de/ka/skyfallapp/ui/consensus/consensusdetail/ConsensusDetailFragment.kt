7865ec1223c610938199cf0730ff50d3aa0da034
Thomas Hofmann <thomas.hofmann@jamitlabs.com>
2019-03-27 16:44:53 +0100

    Improve details styling

--- app/src/main/java/de/ka/skyfallapp/ui/consensus/consensusdetail/ConsensusDetailFragment.kt
+++ app/src/main/java/de/ka/skyfallapp/ui/consensus/consensusdetail/ConsensusDetailFragment.kt
@@ -49,6 +49,12 @@ class ConsensusDetailFragment :
             animate().scaleX(1.0f).scaleY(1.0f)
         }
 
+        getBinding()?.voters?.apply {
+            scaleX = 0.0f
+            scaleY = 0.0f
+            animate().scaleX(1.0f).scaleY(1.0f)
+        }
+
         getBinding()?.topCard?.let { ViewCompat.setTransitionName(it, consensusId) }
         sharedElementEnterTransition =
             TransitionInflater.from(requireContext()).inflateTransition(R.transition.shared_element_cons_transition)
